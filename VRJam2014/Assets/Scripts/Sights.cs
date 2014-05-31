b5981ce0a2edb41d8c14f9b4fcc932f78cc1a3af
Mark Sullivan <maniacbrainiac50@yahoo.com>
2014-05-31 14:54:42 -0400

    working sights

---
+++ VRJam2014/Assets/Scripts/Sights.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sights : MonoBehaviour {
+
+	public GameObject sightPrefab;
+
+	GameObject spawned;
+
+	// Use this for initialization
+	void Start () {
+		spawned = Instantiate (sightPrefab) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		bool didHit = Physics.Raycast (SteamVR_Camera.Instance.offset.position, SteamVR_Camera.Instance.offset.forward, out hit);
+		if (didHit) {
+						spawned.transform.position = hit.point;
+			spawned.SetActive(true);
+				} else {
+			spawned.SetActive(false);
+				}
+	}
+}
