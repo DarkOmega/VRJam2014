8fea532a85f9fed7aa82bd23b7b8e6fd48cffe1e
Mark Sullivan <maniacbrainiac50@yahoo.com>
2014-05-31 12:58:03 -0400

    Merge branch 'master' of https://github.com/DarkOmega/VRJam2014
    Conflicts:
    	VRJam2014/ProjectSettings/AudioManager.asset
    	VRJam2014/ProjectSettings/DynamicsManager.asset
    	VRJam2014/ProjectSettings/EditorBuildSettings.asset
    	VRJam2014/ProjectSettings/EditorSettings.asset
    	VRJam2014/ProjectSettings/GraphicsSettings.asset
    	VRJam2014/ProjectSettings/InputManager.asset
    	VRJam2014/ProjectSettings/NavMeshLayers.asset
    	VRJam2014/ProjectSettings/NetworkManager.asset
    	VRJam2014/ProjectSettings/Physics2DSettings.asset
    	VRJam2014/ProjectSettings/ProjectSettings.asset
    	VRJam2014/ProjectSettings/QualitySettings.asset
    	VRJam2014/ProjectSettings/TagManager.asset
    	VRJam2014/ProjectSettings/TimeManager.asset

---
+++ VRJam2014/Assets/Scripts/KeyboardTranslation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardTranslation : MonoBehaviour {
+
+	float movementSpeed = 1.0f;
+	SteamVR_Camera vrCam;
+
+	// Use this for initialization
+	void Start () {
+		vrCam = GetComponent<SteamVR_Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 deltaPos = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			deltaPos.x -= movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			deltaPos.x += movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			deltaPos.z += movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			deltaPos.z -= movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.E)) 
+		{
+			deltaPos.y += movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.C)) 
+		{
+			deltaPos.y -= movementSpeed;
+		}
+
+		deltaPos *= Time.deltaTime;
+
+		deltaPos = vrCam.offset.rotation * deltaPos;
+
+		transform.position += deltaPos;
+	}
+}
