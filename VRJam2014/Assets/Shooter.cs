8e382f363712e5ca64eaf7ba72481fcfc8682932
adagar <nick@NGarza-C4.local>
2014-05-31 13:02:33 -0400

    Nick Test Scene and Script

---
+++ VRJam2014/Assets/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public Camera MyCamera; 
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody.AddForce(-80,80,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MyCamera.transform.localEulerAngles.Set(this.transform.localEulerAngles);
+	}
+}
